0f0081d5e8363588fbe4656d8653db9df8065851
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-11 21:19:23 +0300

    Update 3-way-merge.cpp

--- 3-way-merge.cpp
+++ 3-way-merge.cpp
@@ -9,4 +9,7 @@ vector<int> threeWayMerge(vector<int> array1, vector<int> array2, vector<int> ar
         int counterarrayThree = 0;
         int counter = 0;
         while (counterarrayOne < array1.size() && (counterarrayTwo < array2.size() && counterarrayThree < array3.size()))
+        {
+                if (array1[counterarrayOne] < array2[counterarrayTwo])
+        }
 }

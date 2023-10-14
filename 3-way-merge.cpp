0c5f94a3cf190d7aa400533a9220492b8d15816a
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-10-14 21:41:37 +0300

    Update 3-way-merge.cpp

--- 3-way-merge.cpp
+++ 3-way-merge.cpp
@@ -61,4 +61,9 @@ int main()
         vector<int> arr2 = {15, 25, 35, 45, 55, 65, 75};
         vector<int> arrr3 ={17, 27, 37, 47, 57, 67, 77, 87,97};
         vector<int> sortedarr = threeWayMerge(arr3, arr2, arr1);
+
+        for(int i -0; i< sortedarr.size(); i++)
+        {
+                
+        }
 }
